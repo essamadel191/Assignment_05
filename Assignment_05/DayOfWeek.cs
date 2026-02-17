535ba16c49d5c01cba34cca5d772f7b0426ec6e3
essamadel191 <essam.adel191@gmail.com>
2026-02-18 00:55:36 +0200

    Add project files.

---
+++ Assignment_05/DayOfWeek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_05
+{
+    enum DayOfWeek
+    {
+        Saturday = 0,
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 3,
+        Wednesday = 4,
+        Thursday = 5,
+        Friday = 6,
+
+    }
+}
---
+++ Assignment_05/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Assignment_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("╔════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                      G-NET-12-C#-4                                 ║");
+            Console.WriteLine("║                      6 Questions                                  ║");
+            Console.WriteLine("╚════════════════════════════════════════════════════════════════════╝\n");
+
+            #region Enums Q1
+
+            //Console.Write("Enter a day number (0-6): ");
+            //int dayNumber = int.Parse(Console.ReadLine());
+            //DayOfWeek day = (DayOfWeek)dayNumber;
+            //Console.WriteLine($"Day: {day}");
+
+            //string weekend = dayNumber switch
+            //{
+            //    0 => "Weekend",
+            //    6 => "Weekend",
+            //    _ => "Workday"
+            //};
+            //Console.WriteLine($"It's The {weekend}.");
+
+            #endregion
+
+            #region Arrays Q1
+
+            //Console.Write("Enter array size: ");
+            //int arraySize = int.Parse(Console.ReadLine());
+            //int[] elements = new int[arraySize];
+
+            //int sum = 0;
+            //decimal average = 0;
+            //int max = 0; 
+            //int min = 0;
+
+            //int[] reverseElements = new int[arraySize];
+
+            //for (int i = 0; i < arraySize; i++)
+            //{
+            //    Console.Write($"Enter element {i}: ");
+            //    elements[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < arraySize; i++)
+            //{
+            //    sum += elements[i];
+            //    average = (decimal)sum / arraySize;
+            //    if (i == 0)
+            //    {
+            //        max = elements[i];
+            //        min = elements[i];
+            //    }
+            //    else
+            //    {
+            //        if (elements[i] > max)
+            //            max = elements[i];
+            //        if (elements[i] < min)
+            //            min = elements[i];
+            //    }
+            //    reverseElements[arraySize - 1 - i] = elements[i];
+            //}
+
+            //Console.WriteLine($"Sum: {sum}");
+            //Console.WriteLine($"Average: {average}");
+            //Console.WriteLine($"Max: {max}");
+            //Console.WriteLine($"Min: {min}");
+            //Console.WriteLine("Reversed Array: " + string.Join(", ", reverseElements));
+
+            #endregion
+
+            #region Arrays Q2
+
+
+
+            #endregion
+        }
+    }
+}
